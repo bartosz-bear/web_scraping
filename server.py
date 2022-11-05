d258798411e3cf8b22d0a1b76887b477bd6e189d
bartosz-bear <bartosz.artur.piechnik@gmail.com>
2022-11-05 14:25:12 +0100

    Updated server from localhost to external

--- server.py
+++ server.py
@@ -95,7 +95,10 @@ def submit():
         )
 
 
+run(host='0.0.0.0', port=8080)
 
+"""
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
     run(host='0.0.0.0', port=port, debug=True)
+"""
